b356a231e86cc285674f35099e2e6367f0665127
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-09-21 21:37:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<!-- template最外层只能包含一个标签 -->
+  	<div>
+          
+    </div>
+</template>
+
+<script>
+// 必须暴露一个对象，对象是组件的配置
+export default {
+
+}
+</script>
+
+<style>
+/* 组件样式，会污染全局 */
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+
+import App from "@/App.vue"
+
+import VueRouter from 'vue-router'
+
+import Login from '@/pages/login'
+
+
+Vue.use(VueRouter)
+var routes=[
+    {path:'/login',component:Login}
+]
+var router=new VueRouter({
+    routes
+})
+
+
+new Vue({
+    el: "#app",
+    router,
+    render: function(createElement){
+        return createElement(App)
+    }
+})
